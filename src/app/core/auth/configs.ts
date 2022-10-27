10de34c0d86266340a7b26cb312056b9682b08d1
xMcLOVINx <csmatheus10@gmail.com>
2022-10-27 01:30:36 -0300

    feat: add auth configs

---
+++ src/app/core/auth/configs.ts
@@ -0,0 +1,7 @@
+export const authConfigs: any = {
+  authority: 'http://localhost:4000',
+  autoLoginRedirectTo: '/',
+  useRefreshToken: true,
+  intervalSecondsToVerifyToken: 3,
+  secondsRemainingToRefreshToken: 60 * 10,
+};
